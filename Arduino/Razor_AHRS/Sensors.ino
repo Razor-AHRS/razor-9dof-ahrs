34a85e34defaeb1fa831277ce2669ec5bef2f866
Peter Bartz <peter-bartz@gmx.de>
2016-02-07 20:34:26 +0100

    Fixed merge to work with 16 bit again

--- Arduino/Razor_AHRS/Sensors.ino
+++ Arduino/Razor_AHRS/Sensors.ino
@@ -3,9 +3,9 @@
 // I2C code to read the sensors
 
 // Sensor I2C addresses
-#define ACCEL_ADDRESS ((uint16_t) 0x53) // 0x53 = 0xA6 / 2
-#define MAGN_ADDRESS  ((uint16_t) 0x1E) // 0x1E = 0x3C / 2
-#define GYRO_ADDRESS  ((uint16_t) 0x68) // 0x68 = 0xD0 / 2
+#define ACCEL_ADDRESS ((int16_t) 0x53) // 0x53 = 0xA6 / 2
+#define MAGN_ADDRESS  ((int16_t) 0x1E) // 0x1E = 0x3C / 2
+#define GYRO_ADDRESS  ((int16_t) 0x68) // 0x68 = 0xD0 / 2
 
 // Arduino backward compatibility macros
 #if ARDUINO >= 100
